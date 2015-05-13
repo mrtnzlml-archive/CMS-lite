f7ab0491669109d2a081371656f62d8ee43fb853
Martin Zlámal <mrtnzlml@gmail.com>
2015-05-13 23:40:36 +0200

    added example shortcode (component)

--- app/components/MainMenu/MainMenu.php
+++ app/components/MainMenu/MainMenu.php
@@ -10,10 +10,13 @@ class MainMenu extends Nette\Application\UI\Control
 	private $items = [];
 	private $templatePath = NULL;
 
-	public function render()
+	public function render(array $parameters = NULL)
 	{
+		if ($parameters) {
+			$this->template->parameters = Nette\Utils\ArrayHash::from($parameters);
+		}
 		$this->template->items = $this->items;
-		$this->template->render($this->templatePath ?: __DIR__ . '/MainMenu.latte');
+		$this->template->render($this->templatePath ?: __DIR__ . '/templates/MainMenu.latte');
 	}
 
 	public function addMainMenuItem(MainMenuItem $item)
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,12 +5,28 @@ namespace App\Presenters;
 use App\Components\MainMenu\IMainMenuFactory;
 use Nette;
 
-
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 
 	/** @var IMainMenuFactory @inject */
 	public $mainMenuFactory;
+
+	public function beforeRender()
+	{
+		$texy = new \Texy();
+
+		//What about XSS?
+		$textFormDb = '<b>Toto je komponenta vložená z databáze:</b> {control mainMenu, param => 42}';
+
+		$latte = new \Latte\Engine;
+		$latte->setLoader(new \Latte\Loaders\StringLoader);
+		$latte->onCompile[] = function ($latte) {
+			\Nette\Bridges\ApplicationLatte\UIMacros::install($latte->getCompiler());
+		};
+		$rendered = $latte->renderToString($textFormDb, ['_control' => $this]);
+
+		$this->template->text = Nette\Utils\Html::el()->setHtml($texy->process($rendered));
+	}
 
 	public function createComponentMainMenu()
 	{
