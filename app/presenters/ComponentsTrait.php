0480851f5ef55ab04477f6c47eb2c7674bbcd405
Martin Zlámal <mrtnzlml@gmail.com>
2015-05-15 12:14:17 +0200

    removed unnecessary comments --- just pure magic... (-:

--- app/presenters/ComponentsTrait.php
+++ app/presenters/ComponentsTrait.php
@@ -7,41 +7,21 @@ trait ComponentsTrait
 
 	use Kdyby\Autowired\AutowireComponentFactories;
 
-	/**
-	 * @param Components\MainMenu\IMainMenuFactory $factory
-	 *
-	 * @return Components\MainMenu\MainMenu
-	 */
 	protected function createComponentMainMenu(Components\MainMenu\IMainMenuFactory $factory)
 	{
 		return $factory->create();
 	}
 
-	/**
-	 * @param Components\ContactForm\IContactFormFactory $factory
-	 *
-	 * @return Components\ContactForm\ContactForm
-	 */
 	protected function createComponentContactForm(Components\ContactForm\IContactFormFactory $factory)
 	{
 		return $factory->create();
 	}
 
-	/**
-	 * @param Components\Css\ICssFactory $factory
-	 *
-	 * @return Components\Css\Css
-	 */
 	protected function createComponentCss(Components\Css\ICssFactory $factory)
 	{
 		return $factory->create();
 	}
 
-	/**
-	 * @param Components\Js\IJsFactory $factory
-	 *
-	 * @return Components\Js\Js
-	 */
 	protected function createComponentJs(Components\Js\IJsFactory $factory)
 	{
 		return $factory->create();
