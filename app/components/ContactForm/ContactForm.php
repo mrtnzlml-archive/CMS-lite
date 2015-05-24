2debb16de39548f756dd6bf3eb04794af760ab77
Martin Zlámal <mrtnzlml@gmail.com>
2015-05-24 14:53:30 +0200

    added ITranslator into ContactForm
    It's going to be real pain...

--- app/components/ContactForm/ContactForm.php
+++ app/components/ContactForm/ContactForm.php
@@ -3,12 +3,23 @@
 namespace App\Components\ContactForm;
 
 use App\Components\AControl;
+use Kdyby\Translation\ITranslator;
+use Kdyby\Translation\Phrase;
+use Kdyby\Translation\Translator;
 use Nette;
 use Nette\Application\UI;
 use Nette\Utils\ArrayHash;
 
 class ContactForm extends AControl
 {
+
+	/** @var Translator */
+	private $translator;
+
+	public function __construct(ITranslator $translator = NULL)
+	{
+		$this->translator = $translator;
+	}
 
 	public function render(array $parameters = NULL)
 	{
@@ -21,11 +32,12 @@ class ContactForm extends AControl
 	public function createComponentForm()
 	{
 		$form = new UI\Form;
+		$form->setTranslator($this->translator->domain('contactForm'));
 		$form->addText('name');
 		$form
 			->addText('email')
 			->setType('email')
-			->addRule(UI\Form::EMAIL);
+			->addRule(UI\Form::EMAIL, new Phrase('useValidEmail'));
 		$form
 			->addTextArea('message')
 			->setRequired();
