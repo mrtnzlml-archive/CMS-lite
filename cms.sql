af7a72fba7e7828e80618b9a9531bfea9a978e94
Martin Zlámal <mrtnzlml@gmail.com>
2015-06-15 09:54:26 +0200

    added empty SQL dump
    just in case you need it

---
+++ cms.sql
@@ -0,0 +1,290 @@
+-- Adminer 4.2.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the article',
+  `slug` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Webalized title (slug) of the article',
+  `body` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Body of the article',
+  `created_at` datetime NOT NULL COMMENT 'Date of the article creation',
+  `published_at` datetime NOT NULL COMMENT 'Date of the article publication',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `article_user`;
+CREATE TABLE `article_user` (
+  `article_id` int(11) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`article_id`,`role`),
+  KEY `IDX_3DD151487294869C` (`article_id`),
+  KEY `IDX_3DD1514857698A6A` (`role`),
+  CONSTRAINT `FK_3DD1514857698A6A` FOREIGN KEY (`role`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_3DD151487294869C` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `deliveries`;
+CREATE TABLE `deliveries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `delivery_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Identifier of the delivery',
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Human readable name of the delivery',
+  `delivery_price` decimal(10,2) NOT NULL COMMENT 'Delivery price including VAT!',
+  `heureka` tinyint(1) NOT NULL COMMENT 'Heureka acceptable?',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_6F07856812136921` (`delivery_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `deliveries` (`id`, `delivery_id`, `name`, `delivery_price`, `heureka`) VALUES
+(419,	'CESKA_POSTA',	'Česká pošta',	166.90,	1),
+(420,	'CESKA_POSTA_NA_POSTU',	'Česká pošta - Balík Na poštu',	122.60,	1),
+(421,	'CSAD_LOGISTIK_OSTRAVA',	'ČSAD Logistik Ostrava',	196.00,	1),
+(422,	'DPD',	'DPD',	170.70,	1),
+(423,	'DHL',	'DHL',	137.70,	1),
+(424,	'DSV',	'DSV',	200.90,	1),
+(425,	'EMS',	'EMS',	178.10,	1),
+(426,	'FOFR',	'FOFR',	169.00,	1),
+(427,	'GEBRUDER_WEISS',	'Gebrüder Weiss',	149.30,	1),
+(428,	'GEIS',	'Geis',	239.00,	1),
+(429,	'GENERAL_PARCEL',	'General Parcel',	111.40,	1),
+(430,	'GLS',	'GLS',	124.50,	1),
+(431,	'HDS',	'HDS',	158.30,	1),
+(432,	'HEUREKAPOINT',	'HeurekaPoint',	135.00,	1),
+(433,	'INTIME',	'InTime',	223.80,	1),
+(434,	'PPL',	'PPL',	206.90,	1),
+(435,	'RADIALKA',	'Radiálka',	198.40,	1),
+(436,	'SEEGMULLER',	'Seegmuller',	174.90,	1),
+(437,	'TNT',	'TNT',	200.10,	1),
+(438,	'TOPTRANS',	'TOPTRANS',	198.20,	1),
+(439,	'UPS',	'UPS',	226.10,	1),
+(440,	'VLASTNI_PREPRAVA',	'Vlastní přeprava',	101.80,	1);
+
+DROP TABLE IF EXISTS `options`;
+CREATE TABLE `options` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `key` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Key of the option',
+  `value` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Value of the option',
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description of the option (form label)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_D035FA874E645A7E` (`key`),
+  KEY `IDX_D035FA8712469DE2` (`category_id`),
+  CONSTRAINT `FK_D035FA8712469DE2` FOREIGN KEY (`category_id`) REFERENCES `option_categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `options` (`id`, `category_id`, `key`, `value`, `description`) VALUES
+(58,	39,	'site_title',	'ANTstudio CMS',	'Název webu'),
+(59,	39,	'site_title_separator',	'|',	'Oddělovač titulku'),
+(60,	40,	'index',	'1',	'Indexovat web');
+
+DROP TABLE IF EXISTS `option_categories`;
+CREATE TABLE `option_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the option category',
+  `full_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Nice name of the option category',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_D79A5E585E237E06` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `option_categories` (`id`, `name`, `full_name`) VALUES
+(39,	'general',	'Obecné'),
+(40,	'seo',	'SEO');
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the article',
+  `slug` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Webalized title (slug) of the article',
+  `body` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Body of the article',
+  `created_at` datetime NOT NULL COMMENT 'Date of the article creation',
+  `published_at` datetime DEFAULT NULL COMMENT 'Date of the article publication',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `page_categories`;
+CREATE TABLE `page_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the page category',
+  `created_at` datetime NOT NULL COMMENT 'Date of the page category creation',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `page_page_category`;
+CREATE TABLE `page_page_category` (
+  `page_id` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  PRIMARY KEY (`page_id`,`category`),
+  KEY `IDX_17096EDAC4663E4` (`page_id`),
+  KEY `IDX_17096EDA64C19C1` (`category`),
+  CONSTRAINT `FK_17096EDA64C19C1` FOREIGN KEY (`category`) REFERENCES `page_categories` (`id`),
+  CONSTRAINT `FK_17096EDAC4663E4` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `page_user`;
+CREATE TABLE `page_user` (
+  `page_id` int(11) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`page_id`,`role`),
+  KEY `IDX_A57CA93C4663E4` (`page_id`),
+  KEY `IDX_A57CA9357698A6A` (`role`),
+  CONSTRAINT `FK_A57CA9357698A6A` FOREIGN KEY (`role`) REFERENCES `users` (`id`),
+  CONSTRAINT `FK_A57CA93C4663E4` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the product',
+  `slug` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Webalized title (slug) of the product',
+  `description` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'Long description of the product',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `product_delivery`;
+CREATE TABLE `product_delivery` (
+  `product_id` int(11) NOT NULL,
+  `delivery` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`,`delivery`),
+  KEY `IDX_FC3186874584665A` (`product_id`),
+  KEY `IDX_FC3186873781EC10` (`delivery`),
+  CONSTRAINT `FK_FC3186873781EC10` FOREIGN KEY (`delivery`) REFERENCES `deliveries` (`id`),
+  CONSTRAINT `FK_FC3186874584665A` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `product_variants`;
+CREATE TABLE `product_variants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `variant_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the product variant (e.g. red-wool).',
+  `sku` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'SKU – Stock Keeping Unit',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_78283976F9038C4` (`sku`),
+  KEY `IDX_782839764584665A` (`product_id`),
+  CONSTRAINT `FK_782839764584665A` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `product_variant_variant_value`;
+CREATE TABLE `product_variant_variant_value` (
+  `productVariant_id` int(11) NOT NULL,
+  `variantValue` int(11) NOT NULL,
+  PRIMARY KEY (`productVariant_id`,`variantValue`),
+  KEY `IDX_303C2AAE5708BDEF` (`productVariant_id`),
+  KEY `IDX_303C2AAE6E6B91BE` (`variantValue`),
+  CONSTRAINT `FK_303C2AAE5708BDEF` FOREIGN KEY (`productVariant_id`) REFERENCES `product_variants` (`id`),
+  CONSTRAINT `FK_303C2AAE6E6B91BE` FOREIGN KEY (`variantValue`) REFERENCES `variant_values` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `resources`;
+CREATE TABLE `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Identifier of the role',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `resources` (`id`, `name`) VALUES
+(77,	'Admin:Dashboard'),
+(78,	'Admin:Eshop'),
+(79,	'Admin:Options'),
+(80,	'Admin:Page');
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Identifier of the role',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_B63E2EC7727ACA70` (`parent_id`),
+  CONSTRAINT `FK_B63E2EC7727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `roles` (`id`, `parent_id`, `name`) VALUES
+(77,	NULL,	'guest'),
+(78,	77,	'user'),
+(79,	78,	'admin'),
+(80,	79,	'superadmin');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'User''s email',
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'User''s password',
+  `created_at` datetime NOT NULL COMMENT 'Date of user account creation',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `users` (`id`, `email`, `password`, `created_at`) VALUES
+(39,	'admin@antstudio.cz',	'$2y$10$ojRTT3fNz9iY4y377458D.XtPKmfIxJ48jgGkIwkyqUlfESK93O8a',	'2015-06-15 09:51:55'),
+(40,	'demo@antstudio.cz',	'$2y$10$.ffqnKfPQNiDyYkQaH2BkewgUaTxMHnzBZRDgyo8wk3q9DMBFQD0O',	'2015-06-15 09:51:55');
+
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `user_id` int(11) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role`),
+  KEY `IDX_2DE8C6A3A76ED395` (`user_id`),
+  KEY `IDX_2DE8C6A357698A6A` (`role`),
+  CONSTRAINT `FK_2DE8C6A357698A6A` FOREIGN KEY (`role`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FK_2DE8C6A3A76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `user_role` (`user_id`, `role`) VALUES
+(39,	80),
+(40,	78);
+
+DROP TABLE IF EXISTS `variants`;
+CREATE TABLE `variants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the variant (e.g. color, size, material).',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_B39853E15E237E06` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `variants` (`id`, `name`) VALUES
+(58,	'barva'),
+(60,	'materiál'),
+(59,	'velikost');
+
+DROP TABLE IF EXISTS `variant_values`;
+CREATE TABLE `variant_values` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `variant_id` int(11) DEFAULT NULL,
+  `value` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Value of the variant (e.g. red, medium, wool).',
+  PRIMARY KEY (`id`),
+  KEY `IDX_2FFDEB303B69A9AF` (`variant_id`),
+  CONSTRAINT `FK_2FFDEB303B69A9AF` FOREIGN KEY (`variant_id`) REFERENCES `variants` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `variant_values` (`id`, `variant_id`, `value`) VALUES
+(172,	58,	'modrá'),
+(173,	58,	'červená'),
+(174,	58,	'žlutá'),
+(175,	59,	'S'),
+(176,	59,	'M'),
+(177,	59,	'L'),
+(178,	60,	'bavlna'),
+(179,	60,	'dřevo'),
+(180,	60,	'polyester');
+
+-- 2015-06-15 07:52:49
