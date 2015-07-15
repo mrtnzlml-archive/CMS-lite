5ad6df08bca80721755f89b65b1b3bee5b95ad1d
Martin Zlámal <mrtnzlml@gmail.com>
2015-07-15 14:04:58 +0200

    Add ability to persist and remove navigation

--- libs/Navigation/components/MenuEditor/MenuEditor.php
+++ libs/Navigation/components/MenuEditor/MenuEditor.php
@@ -2,8 +2,11 @@
 
 namespace Navigation;
 
+use App\Components\Flashes\Flashes;
 use Kdyby\Doctrine\EntityManager;
+use Nette\Application\UI;
 use Nette\Application\UI\Control;
+use Nette\Utils\ArrayHash;
 
 class MenuEditor extends Control
 {
@@ -24,8 +27,38 @@ class MenuEditor extends Control
 	{
 		$adminRoot = $this->em->getRepository(NavigationItem::class)->findOneBy(['name' => NavigationFacade::ROOT_ADMIN]);
 		$this->template->adminMenu = $this->navigationFacade->getItemTreeBelow($adminRoot->getId());
-		$this->template->navigations = $this->em->getRepository(Navigation::class)->findAll();
 		$this->template->render(__DIR__ . '/templates/MenuEditor.latte');
+	}
+
+	public function createComponentNavigationCreate()
+	{
+		$form = new UI\Form;
+		$form->addText('navigation', 'Název nové navigace')
+			->setRequired('Zadejte prosím název nové navigace');
+		$form->addSubmit('create', 'Vytvořit navigaci');
+		$form->onSuccess[] = function (UI\Form $_, ArrayHash $values) {
+			$navigation = (new Navigation)->setName($values->navigation);
+			$this->em->persist($navigation);
+			$this->em->flush($navigation);
+			$this->presenter->flashMessage('Nové menu bylo úspěšně vytvořeno.', Flashes::FLASH_SUCCESS);
+			$this->redirect('this');
+		};
+		return $form;
+	}
+
+	public function createComponentNavigationSelect()
+	{
+		$form = new UI\Form;
+		$form->addSelect('navigation', 'Navigace', $this->em->getRepository(Navigation::class)->findPairs('name'));
+		$form->addSubmit('delete', 'Smazat zvolenou navigaci');
+		$form->onSuccess[] = function (UI\Form $_, ArrayHash $values) {
+			$partialEntity = $this->em->getPartialReference(Navigation::class, $values->navigation);
+			$this->em->remove($partialEntity);
+			$this->em->flush($partialEntity);
+			$this->presenter->flashMessage('Navigace byla úspěšně smazána.', Flashes::FLASH_SUCCESS);
+			$this->redirect('this');
+		};
+		return $form;
 	}
 
 	/**
