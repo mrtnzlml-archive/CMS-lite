115e4c1082c81443c1be4bd0bd71f83bf63f0ac9
Martin Zlámal <mrtnzlml@gmail.com>
2015-07-17 08:04:25 +0200

    Allow delete users

--- libs/Users/components/UsersGrid/UsersGrid.php
+++ libs/Users/components/UsersGrid/UsersGrid.php
@@ -3,6 +3,7 @@
 namespace Users\Components\UsersGrid;
 
 use App\Components\AControl;
+use App\Components\Flashes\Flashes;
 use Kdyby\Doctrine\EntityManager;
 use Nette;
 use Nette\Application\UI;
@@ -28,6 +29,18 @@ class UsersGrid extends AControl
 		$users = $this->em->getRepository(User::class)->fetch(new UsersQuery);
 		$this->template->users = $users;
 		$this->template->render($this->templatePath ?: __DIR__ . '/templates/UsersGrid.latte');
+	}
+
+	/**
+	 * @secured
+	 */
+	public function handleDeleteUser($id)
+	{
+		$partialUser = $this->em->getPartialReference(User::class, $id);
+		$this->em->remove($partialUser);
+		$this->em->flush($partialUser);
+		$this->presenter->flashMessage('Uživatel byl úspěšně smazán.', Flashes::FLASH_SUCCESS);
+		$this->redirect('this');
 	}
 
 }
