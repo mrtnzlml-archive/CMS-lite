54ecac13d54af909ee25617915e36fdc9b645fde
Miroslav Filipec <filipec.miroslav@gmail.com>
2015-07-22 19:28:13 +0200

    new File entity attributtes

--- custom/Files/File.php
+++ custom/Files/File.php
@@ -20,59 +20,92 @@ use Kdyby\Doctrine\Entities\MagicAccessors;
  * @method string getDescription()
  * @method setUuid(string $uuid)
  * @method string getUuid()
+ * @method setExtension(string)
+ * @method string getExtension()
  * @method setSize(int $size)
  * @method int getSize()
  * @method setType(string $type)
  * @method string getType()
+ * @method User getAuthor()
+ * @method setAuthor(User $author)
  */
 class File
 {
 
-	use Identifier;
-	use MagicAccessors;
+    use Identifier;
+    use MagicAccessors;
 
-	const TYPE_ATTACHMENT = 'ATTACHMENT';
+    const TYPE_ATTACHMENT = 'ATTACHMENT';
 
-	/**
-	 * @ORM\Column(type="string")
-	 * @var string
-	 */
-	protected $name;
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
 
-	/**
-	 * @ORM\Column(type="string")
-	 * @var string
-	 */
-	protected $sanitizedName;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $name;
 
-	/**
-	 * @ORM\Column(type="string", nullable=TRUE)
-	 * @var string
-	 */
-	protected $title;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $sanitizedName;
 
-	/**
-	 * @ORM\Column(type="text", nullable=TRUE)
-	 * @var string
-	 */
-	protected $description;
+    /**
+     * @ORM\Column(type="string", nullable=TRUE)
+     * @var string
+     */
+    protected $title;
 
-	/**
-	 * @ORM\Column(type="string")
-	 * @var string
-	 */
-	protected $uuid;
+    /**
+     * @ORM\Column(type="text", nullable=TRUE)
+     * @var string
+     */
+    protected $description;
 
-	/**
-	 * @ORM\Column(type="integer")
-	 * @var string
-	 */
-	protected $size;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $uuid;
 
-	/**
-	 * @ORM\Column(type="string")
-	 * @var string
-	 */
-	protected $type = self::TYPE_ATTACHMENT;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $extension;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @var string
+     */
+    protected $size;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $type = self::TYPE_ATTACHMENT;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Users\User", cascade={"persist"})
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * @var \Users\User
+     */
+    protected $author;
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
 
 }
